4b6f5320377c8696a915609fb5c6e6d1f8e71fe3
sheenalskinner <sheenalskinner@gmail.com>
2025-03-05 14:56:19 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -83,18 +83,10 @@ export default function Chat() {
               className="h-20 w-20"
             />
             <h1 className="text-lg font-semibold text-black">
-              Hi, I'm a fine tuned LLM.
+              Hi, I'm Sheena's AI PM assistant!
             </h1>
             <p className="text-gray-500">
-              I'm part of a series of computational experiments taught by{" "}
-              <a
-                href="https://linkin.bio/yallahalim/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="font-medium underline underline-offset-4 transition-colors hover:text-black"
-              >
-                Halim Madi
-              </a>. I was built using{" "}
+              I was built using{" "}
               <a
                 href="https://openai.com/blog/gpt-3-5-turbo-fine-tuning-and-api-updates"
                 target="_blank"
@@ -166,36 +158,6 @@ export default function Chat() {
             )}
           </button>
         </form>
-        <p className="text-center text-xs text-gray-400">
-          Built with{" "}
-          <a
-            href="https://sdk.vercel.ai/docs"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="transition-colors hover:text-black"
-          >
-            Vercel AI SDK
-          </a>
-          ,{" "}
-          <a
-            href="https://openai.com/blog/gpt-3-5-turbo-fine-tuning-and-api-updates"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="transition-colors hover:text-black"
-          >
-            OpenAI GPT-3.5-turbo, as part of a course taught by
-          </a>{" "}
-          Halim Madi.{" "}
-          <a
-            href="https://linkin.bio/yallahalim/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="transition-colors hover:text-black"
-          >
-            Learn to build your own
-          </a>
-          .
-        </p>
       </div>
     </main>
   );
